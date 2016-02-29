1ee8254602ed5754d378d7820b6e596067cbbf9d
filin-from-2042 <filin2042@gmail.com>
2016-02-29 14:14:07 +0300

    поправлен конфиг

--- config.php
+++ config.php
@@ -4,22 +4,22 @@ define('HTTP_SERVER', 'http://elstore/');
 
 // HTTPS
 define('HTTPS_SERVER', 'http://elstore/');
-
+$dir=getcwd();
 // DIR
-define('DIR_APPLICATION', 'H:\open2\OpenServer\domains\elstore\catalog/');
-define('DIR_SYSTEM', 'H:\open2\OpenServer\domains\elstore\system/');
-define('DIR_DATABASE', 'H:\open2\OpenServer\domains\elstore\system/database/');
-define('DIR_LANGUAGE', 'H:\open2\OpenServer\domains\elstore\catalog/language/');
-define('DIR_TEMPLATE', 'H:\open2\OpenServer\domains\elstore\catalog/view/theme/');
-define('DIR_CONFIG', 'H:\open2\OpenServer\domains\elstore\system/config/');
-define('DIR_IMAGE', 'H:\open2\OpenServer\domains\elstore\image/');
-define('DIR_CACHE', 'H:\open2\OpenServer\domains\elstore\system/cache/');
-define('DIR_DOWNLOAD', 'H:\open2\OpenServer\domains\elstore\download/');
-define('DIR_LOGS', 'H:\open2\OpenServer\domains\elstore\system/logs/');
+define('DIR_APPLICATION', $dir.'\catalog/');
+define('DIR_SYSTEM', $dir.'\system/');
+define('DIR_DATABASE', $dir.'\system/database/');
+define('DIR_LANGUAGE', $dir.'\catalog/language/');
+define('DIR_TEMPLATE', $dir.'\catalog/view/theme/');
+define('DIR_CONFIG', $dir.'\system/config/');
+define('DIR_IMAGE', $dir.'\image/');
+define('DIR_CACHE', $dir.'\system/cache/');
+define('DIR_DOWNLOAD', $dir.'\download/');
+define('DIR_LOGS', $dir.'\system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', '192.168.1.37');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'elstore');
