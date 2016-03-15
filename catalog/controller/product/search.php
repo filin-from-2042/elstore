9d091b408f53fce46065bc6d65c314565a014b5d
Eugeniya <linansk@inbox.ru>
2016-03-15 22:05:06 +0300

    добавлена картинка по умолчанию

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -222,7 +222,8 @@ class ControllerProductSearch extends Controller {
 				if ($result['image']) {
 					$image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
 				} else {
-					$image = false;
+                    $image = 'no_image.jpg';
+                    $image= $this->model_tool_image->resize($image, $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
 				}
 				
 				if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
