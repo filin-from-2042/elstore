6a47099817060661f3361862807e96f19bc29bfd
filin-from-2042 <filin2042@gmail.com>
2017-07-25 20:30:22 +0300

    ссылка на главную в крошках смены пароля личного кабинета

--- catalog/controller/account/password.php
+++ catalog/controller/account/password.php
@@ -26,7 +26,7 @@ class ControllerAccountPassword extends Controller {
       	$this->data['breadcrumbs'] = array();
 
       	$this->data['breadcrumbs'][] = array(
-        	'text'      => $this->language->get('text_home'),
+        	'text'      => $this->language->get('icon_home'),
 			'href'      => $this->url->link('common/home'),       	
         	'separator' => false
       	); 
