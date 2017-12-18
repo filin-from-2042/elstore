e4bf0c7a49fcf90759ddfdec447b0dc1c88de6c9
filin-from-2042 <filin2042@gmail.com>
2017-12-18 09:43:52 +0300

    !FIX проверка кэша категорий

--- catalog/model/catalog/category.php
+++ catalog/model/catalog/category.php
@@ -11,7 +11,7 @@ class ModelCatalogCategory extends Model {
         $cStoreId = (int)$this->config->get('config_store_id');
         $cLanguageId = (int)$this->config->get('config_language_id');
         $categories = $this->cache->get('categories.'.$parent_id.'.' . $cStoreId . '.' . $cLanguageId);
-        if(!$categories){
+        if($categories===null){
 		    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . $cLanguageId . "' AND c2s.store_id = '" . $cStoreId . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
             $categories = $query->rows;
             $this->cache->set('categories.'.$parent_id.'.' . $cStoreId . '.' . $cLanguageId, $categories);
